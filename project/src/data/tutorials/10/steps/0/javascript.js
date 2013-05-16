1a33feb5b0c2d6a78e300202ced217fbc42550f5
Rich Harris <richard.a.harris@gmail.com>
2013-05-16 11:35:50 +0100

    work on SVG tutorial

--- project/src/data/tutorials/10/steps/0/javascript.js
+++ project/src/data/tutorials/10/steps/0/javascript.js
@@ -1,9 +1,95 @@
+var viewBoxes = {
+	normal: { x: 0, y: 0, width: 819.18, height: 596.441 },
+	neuron: { x: 0, y: 100, width: 560, height: 407.451 },
+	axon: { x: 289, y: 195, width: 530, height: 385.623 },
+	synapse: { x: 190, y: 0, width: 525, height: 381.985 }
+};
+
 var view = new Ractive({
   el: output,
-  template: template
+  template: template,
+  data: { viewBox: viewBoxes.normal }
 });
 
-var hotspots = [], hotspotNodes, hotspotOver, i;
+setTimeout( function () {
+	view.set( 'hotspotsVisible', true );
+}, 1500 );
+
+
+var info, closeup;
+
+view.on({
+  reset: function () {
+    this.set({
+      info: null,
+      closeup: null
+    });
+  },
+
+  moreInfo: function ( event, el ) {
+    var hotspotId = el.getAttribute( 'data-hotspot' );
+
+    this.set( 'info', hotspotId );
+    this.nodes[ hotspotId + '-label' ].style.opacity = 1;
+  },
+
+  showCloseUp: function ( event, el ) {
+    this.set( 'closeup', el.getAttribute( 'data-closeup' ) );
+  },
+
+  'set:info': function ( newInfo ) {
+    if ( newInfo !== info ) {
+      if ( info ) {
+        this.nodes[ info + '-label' ].style.opacity = 0;
+      }
+
+      info = newInfo;
+    }
+
+    if ( info ) {
+      this.nodes[ info + '-label' ].style.opacity = 1;
+      this.set( 'hotspotsVisible', false );
+    } else {
+      this.set( 'hotspotsVisible', true );
+    }
+  },
+
+  'set:closeup': function ( newCloseup ) {
+    var viewBox;
+
+    if ( closeup === newCloseup ) {
+      return;
+    }
+
+    // previous
+    if ( closeup ) {
+      this.set( closeup + 'Visible', false );
+      this.set( 'hotspotsVisible', false );
+    }
+
+    // new
+    viewBox = ( newCloseup ? viewBoxes[ newCloseup ] : viewBoxes.normal );
+
+    this.animate( 'viewBox', viewBox, {
+      duration: 300,
+      easing: 'easeInOut',
+      complete: function () {
+        if ( newCloseup ) {
+          view.set( newCloseup + 'Visible', true );
+          view.set( 'hotspotsVisible', false );
+        } else {
+          view.set( 'hotspotsVisible', true );
+        }
+      }
+    });
+
+    closeup = newCloseup;
+  }
+});
+
+
+
+/*var hotspots = [], hotspotNodes, hotspotOver, i;
 
 hotspotNodes = view.el.getElementsByClassName( 'hotspot' );
 i = hotspotNodes.length;
@@ -40,4 +126,4 @@ while ( i-- ) {
 	hotspots[i] = hotspotNodes[i];
 	hotspots[i].addEventListener( 'mouseover', hotspotOver );
 	hotspots[i].addEventListener( 'mouseout', hotspotOut );
-}
+}*/
