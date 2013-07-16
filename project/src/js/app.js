863067cbe3e4ebc8a6b1ecb116f7570fc6de092d
Rich Harris <richard.a.harris@gmail.com>
2013-07-16 17:57:59 +0100

    check if IE8 and show warning

--- project/src/js/app.js
+++ project/src/js/app.js
@@ -8,16 +8,44 @@ define([ 'domReady', 'Statesman', 'controllers/main' ], function ( domReady, Sta
 
 	app = { state: new Statesman() };
 
+	// TEMP
+	app.state.debug = true;
+
 	dataPromise = $.getJSON( 'data.json' );
 
 
 	domReady( function () {
+		var eval2, result, warning;
+
 		app.el = document.getElementById( 'container' );
 
-		dataPromise.then( function ( data ) {
-			app.data = data;
-			main( app );
-		});
+		// test that eval2 will work (it won't in IE8)
+		eval2 = eval;
+
+		(function () {
+			eval2( 'var test = "test"' );
+		}());
+
+		(function () {
+			try {
+				result = eval2( 'test' );
+			} catch ( err ) {
+				// we must be in IE
+			}
+		}());
+
+		if ( result !== 'test' ) {
+			warning = document.createElement( 'div' );
+			warning.innerHTML = '<p>This app will not work in some older browsers that implement <code>eval</code> incorrectly, including this one. Please try again using Chrome or Firefox.</p><p>Note that <span class="logo">Ractive.js</span> itself works in all modern browsers and Internet Explorer 8 and above.</p>'
+			app.el.appendChild( warning ); 
+		}
+
+		else {
+			dataPromise.then( function ( data ) {
+				app.data = data;
+				main( app );
+			});
+		}
 	});
 
 
